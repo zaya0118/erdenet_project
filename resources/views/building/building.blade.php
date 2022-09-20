1a21f4c958de4bc50d7cba690b386f3747f18992
ZSumiyabazar0303 <zorigtsumyabazar@gmail.com>
2022-09-20 12:42:23 +0800

    Update building.blade.php

--- resources/views/building/building.blade.php
+++ resources/views/building/building.blade.php
@@ -40,10 +40,10 @@
                                                     <!--begin::Menu 1-->
                                                     <button type="button" class="btn btn-light-primary me-3" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
                                                     <!--begin::Svg Icon | path: icons/duotune/general/gen031.svg-->
-                                                    <span class="svg-icon svg-icon-2">
+                                                    <!-- <span class="svg-icon svg-icon-2">
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                                             <path d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z" fill="black" />
-                                                        </svg>
+                                                        </svg> -->
                                                     </span>
                                                     <!--end::Svg Icon-->{{trans('display.filter')}}</button>
                                                     <div class="menu menu-sub menu-sub-dropdown w-300px w-md-325px" data-kt-menu="true">
@@ -79,12 +79,12 @@
                                                     <!--begin::Add building-->
                                                     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_add_building">
                                                     <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
-                                                    <span class="svg-icon svg-icon-2">
+                                                    <!-- <span class="svg-icon svg-icon-2">
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                                             <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2" rx="1" transform="rotate(-90 11.364 20.364)" fill="black" />
                                                             <rect x="4.36396" y="11.364" width="16" height="2" rx="1" fill="black" />
                                                         </svg>
-                                                    </span>
+                                                    </span> -->
                                                     <!--end::Svg Icon-->{{trans('display.add_building')}}</button>
                                                     <!--end::Add building-->
                                                 </div>
@@ -288,7 +288,7 @@
                                                     <tr class="text-start text-muted fw-bolder fs-7 text-uppercase gs-0">
                                                         <th class="min-w-125px">1</th>
                                                         <th class="min-w-125px">ААН 924563</th>
-                                                        <th class="min-w-125px">СУКА ПЛАЗА</th>
+                                                        <th class="min-w-125px">ЯЛАЛТ ПЛАЗА</th>
                                                         <th class="min-w-125px">СХД 13-р хороо 12-р байр</th>
                                                         <th class="min-w-125px">+976 77102356</th>
                                                         <th class="min-w-100px">
